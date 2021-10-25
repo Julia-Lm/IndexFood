ee41fbcd04d3d834e9c6171b94147e06f6ed6b3b
Julia-Lm <63656237+Julia-Lm@users.noreply.github.com>
2021-10-25 23:43:19 +0300

    Add adaptive and customize the slider

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slide-button-next',
+      prevEl: '.slide-button-prev',
+    }
+});
